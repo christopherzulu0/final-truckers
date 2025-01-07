61dd0761d72cc128f7b7c4c02f2ac52777bf07c1
christopherzulu0 <christopherzulu04@gmail.com>
2025-01-07 02:33:26 +0200

    error fix

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,17 @@ const nextConfig = {
       dynamic: 30,
     },
   },
+  reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+    // Experimental:{
+    //   appDir: true,
+    // },
+  },
   serverExternalPackages: ["@node-rs/argon2"],
   images: {
     remotePatterns: [
